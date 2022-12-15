9074ffe83016e9d18a4a710a1285c1b009e817a0
Devin Withoft <teddyfest@gmail.com>
2022-12-15 11:45:55 -0700

    Event Cards Working

---
+++ Tower.client/src/pages/EventDetails.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-fluid">
+    <section class="row">
+      <div v-if="event" class="col-12">
+
+        HEY SQUAD
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+import { useRoute } from "vue-router";
+import { eventsService } from "../services/EventsService.js";
+import Pop from "../utils/Pop.js";
+export default {
+  setup() {
+    const route = useRoute();
+    async function getEventById() {
+      try {
+        await eventsService.getEventById(route.params.eventId)
+      } catch (error) {
+        Pop.error(error.message)
+      }
+    }
+    onMounted(() =)
+    return {}
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
