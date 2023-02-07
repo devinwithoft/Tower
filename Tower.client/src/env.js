952d8da70a84e0454ca413fbe309b0d73c2d6d2f
devin withoft <teddyfest@gmail.com>
2023-02-06 19:28:42 -0700

    pointed ENV to appropriate sever location

--- Tower.client/src/env.js
+++ Tower.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:7045' : 'https://github.com/devinwithoft/Tower/tree/main/Tower/server'
+export const baseURL = dev ? 'http://localhost:7045' : 'https://fullstack-tower.onrender.com'
 export const useSockets = false
 export const domain = 'devin-withoft.us.auth0.com'
 export const clientId = 'WGzMo6ywKtk2mwlONShkF4F4RuTLWGds'
